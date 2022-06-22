b6c5181c34e7d934da4558ffba8e4995e349137a
Ji-Xinyou <jerryji0414@outlook.com>
2022-06-22 22:20:20 +0800

    update plot python script

--- record2plot.py
+++ record2plot.py
@@ -2,18 +2,16 @@ import argparse
 import os
 import matplotlib.pyplot as plt
 
-# TODO(Ji-Xinyou): revise the code for parsing new experiment setup
-
 # use this to parse train_loss, other loss is meaning less to be analyzed
-def parse_dict(dict, logpath, keyword):
+def parse_dict(logpath, keyword):
     '''
         Args:
-            - dict: the HashTable to save the history
-                - key: dataset name
-                - value: list of history
             - logpath: the path of logfile
             - keyword: the keyword to find in the file
+
+        Returns the parsed dictionary
     '''
+    d = {}
     with open(logpath, mode='r') as f:
         rows = f.readlines()
         target_word = keyword
@@ -22,8 +20,8 @@ def parse_dict(dict, logpath, keyword):
             if pos != -1:
                 # find dataset name, as key
                 dataset_name = row[:row.find("|")].strip()
-                if dataset_name not in dict:
-                    dict[dataset_name] = []
+                if dataset_name not in d:
+                    d[dataset_name] = []
 
                 pos += len(target_word)
                 row = row[pos:]  # strip all the things before the loss
@@ -33,7 +31,20 @@ def parse_dict(dict, logpath, keyword):
                     row = row[:spacepos]  # strip all things after the loss
 
                 loss = float(row)
-                dict[dataset_name].append(loss)
+                d[dataset_name].append(loss)
+    return d
+
+def average_loss_history(loss_history_dict):
+    keys = list(loss_history_dict.keys())
+    nhist = len(loss_history_dict[keys[0]])
+    hist = []
+    for i in range(nhist):
+        val = 0
+        for key in keys:
+            val += loss_history_dict[key][i]
+        val /= len(keys)
+        hist.append(val)
+    return hist
 
 def draw_plot(x_hist,
               y_hists,
@@ -49,67 +60,64 @@ def draw_plot(x_hist,
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.legend()
-    plt.savefig(os.path.join(args.out_dir, name))
+    plt.savefig(os.path.join('./figures', name))
 
+folder = 'logs/DigitModel-Fedseries'
+datasets = ['mnist', 'kmnist', 'svhn', 'cifar10']
+skews = ['quantity', 'feat_filter', 'feat_noise', 'label_across', 'label_within']
+nclient = '4'
 
-'''
-    In logpos folder, you should have fedbn.log, fedprox.log, fedavg.log
-'''
-parser = argparse.ArgumentParser()
-parser.add_argument("--logpos", type=str, default="./logs")
-parser.add_argument("--out_dir", type=str, default="./fig_folder")
-args = parser.parse_args()
+for dataset in datasets:
+    for skew in skews:
+        if skew == 'feat_noise':
+            continue
+        logname_fedbn = f'fedbn_{dataset}_{skew}_{nclient}.log'
+        logname_fedprox = f'fedprox_{dataset}_{skew}_{nclient}.log'
+        logname_fedavg = f'fedavg_{dataset}_{skew}_{nclient}.log'
 
-folder = args.logpos
+        logfile_fedbn = os.path.join(folder, logname_fedbn)
+        logfile_fedprox = os.path.join(folder, logname_fedprox)
+        logfile_fedavg = os.path.join(folder, logname_fedavg)
 
-loss_history_fedbn = {}
-loss_history_fedprox = {}
-loss_history_fedavg = {}
+        # loss_history_{algorithm}: has key(client 0, client1, client2 and client 3)
+        # we average the loss history of nclient to be our final loss history
+        loss_history_fedbn = parse_dict(logfile_fedbn, "Train Loss: ")
+        loss_history_fedbn = average_loss_history(loss_history_fedbn)
+        acc_history_fedbn = parse_dict(logfile_fedbn, "Test  Acc: ")['server']
 
-tacc_history_fedbn = {}
-tacc_history_fedprox = {}
-tacc_history_fedavg = {}
+        loss_history_fedprox = parse_dict(logfile_fedprox, "Train Loss: ")
+        loss_history_fedprox = average_loss_history(loss_history_fedprox)
+        acc_history_fedprox = parse_dict(logfile_fedprox, "Test  Acc: ")['server']
 
-logfile_fedbn = os.path.join(folder, "fedbn.log")
-logfile_fedprox = os.path.join(folder, "fedprox.log")
-logfile_fedavg = os.path.join(folder, "fedavg.log")
+        loss_history_fedavg = parse_dict(logfile_fedavg, "Train Loss: ")
+        loss_history_fedavg = average_loss_history(loss_history_fedavg)
+        acc_history_fedavg = parse_dict(logfile_fedavg, "Test  Acc: ")['server']
 
-parse_dict(loss_history_fedavg, logfile_fedavg, "Train Loss: ")
-parse_dict(loss_history_fedprox, logfile_fedprox, "Train Loss: ")
-parse_dict(loss_history_fedbn, logfile_fedbn, "Train Loss: ")
+        minlen = min(len(loss_history_fedavg), len(loss_history_fedbn), len(loss_history_fedprox))
+        loss_history_fedavg, loss_history_fedprox, loss_history_fedbn = \
+        loss_history_fedavg[:minlen], loss_history_fedprox[:minlen], loss_history_fedbn[:minlen]
 
-parse_dict(tacc_history_fedavg, logfile_fedavg, "Test  Acc: ")
-parse_dict(tacc_history_fedprox, logfile_fedprox, "Test  Acc: ")
-parse_dict(tacc_history_fedbn, logfile_fedbn, "Test  Acc: ")
+        acc_history_fedavg, acc_history_fedprox, acc_history_fedbn = \
+        acc_history_fedavg[:minlen], acc_history_fedprox[:minlen], acc_history_fedbn[:minlen]
 
-for dname in loss_history_fedbn.keys():
-    # save the plot named dname
-    loss_fedavg = loss_history_fedavg[dname]
-    loss_fedprox = loss_history_fedprox[dname]
-    loss_fedbn = loss_history_fedbn[dname]
+        nepochs = range(1, minlen + 1)
 
-    tacc_fedavg = tacc_history_fedavg[dname]
-    tacc_fedprox = tacc_history_fedprox[dname]
-    tacc_fedbn = tacc_history_fedbn[dname]
+        draw_plot(x_hist=nepochs,
+                y_hists=[loss_history_fedavg,
+                        loss_history_fedprox,
+                        loss_history_fedbn],
+                labels=["FedAvg", "FedProx", "FedBN"],
+                title=f"Loss History on {dataset}_{skew} {nclient} clients",
+                x_label="Global Epochs",
+                y_label="Loss",
+                name=f'Loss_{dataset}_{skew}.png')
 
-    nepochs = range(1, len(loss_fedbn) + 1)
-
-    draw_plot(x_hist=nepochs,
-              y_hists=[loss_fedbn,
-                       loss_fedprox,
-                       loss_fedavg],
-              labels=["FedBN", "FedProx", "FedAvg"],
-              title=f"Loss History on {dname}",
-              x_label="Epochs",
-              y_label="Loss",
-              name=f'Loss_{dname}.png')
-    
-    draw_plot(x_hist=nepochs,
-              y_hists=[tacc_fedbn,
-                       tacc_fedprox,
-                       tacc_fedavg],
-              labels=["FedBN", "FedProx", "FedAvg"],
-              title=f"Test Accuracy History on {dname}",
-              x_label="Epochs",
-              y_label="Accuracy (%)",
-              name=f'Tacc_{dname}.png')
+        draw_plot(x_hist=nepochs,
+                y_hists=[acc_history_fedavg,
+                        acc_history_fedprox,
+                        acc_history_fedbn],
+                labels=["FedAvg", "FedProx", "FedBN"],
+                title=f"Test Accuracy History on {dataset}_{skew} {nclient} clients",
+                x_label="Global Epochs",
+                y_label="Accuracy (%)",
+                name=f'Tacc_{dataset}_{skew}.png')
